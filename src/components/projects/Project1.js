8568e6b8dc171fd3cb5aab3781ebf02fd6dba099
Pedro Revilla <peterevilla@gmail.com>
2020-10-23 23:00:46 -0500

    wip

--- src/components/projects/Project1.js
+++ src/components/projects/Project1.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
+import iphone from '../../images/iphone.svg'
+import Fade from "react-reveal/Fade";
 
 
 const Project1 = () => {
+
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.addEventListener("scroll", (event) => {
+    if (window.scrollY > 400) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  });
   return (
-    <div className="h-screen bg-myblack">
-      <p className="text-mywhite">Project1</p>
+    <div className="h-screen bg-myblack flex flex-col lg:flex-row justify-around  ">
+      <p className="text-mywhite self-center">Project1</p>
+      <div className="mt-20">
+      <Fade right>
+      <Fade right when={isScrolled}>
+      <img src={iphone} alt="alt"/>
+      </Fade>
+      </Fade>
+      </div>
     </div>
   );
 };
